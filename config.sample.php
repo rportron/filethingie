505f3a5977aa2b052950475ecb9c740f135bb70a
rportron <github@portron.org>
2023-03-10 14:29:16 +0100

    Security update
    I prevent the possibility of renaming files and uploading .sql files in the config.php file because there are possible exploits on this system: you upload a .txt file that you can rename to php or sql...

--- config.sample.php
+++ config.sample.php
@@ -13,19 +13,19 @@ function ft_settings_external_load() {
 
   $ft["settings"]["DIR"]               = "."; // Your default directory. Do NOT include a trailing slash!
   $ft["settings"]["LANG"]              = "en"; // Language. Do not change unless you have downloaded language file.
-  $ft["settings"]["MAXSIZE"]           = 2000000; // Maximum file upload size - in bytes.
+  $ft["settings"]["MAXSIZE"]           = 50000000; // Maximum file upload size - in bytes.
   $ft["settings"]["PERMISSION"]        = 0644; // Permission for uploaded files.
   $ft["settings"]["DIRPERMISSION"]     = 0777; // Permission for newly created folders.
   $ft["settings"]["LOGIN"]             = TRUE; // Set to FALSE if you want to disable password protection.
   $ft["settings"]["UPLOAD"]            = TRUE; // Set to FALSE if you want to disable file uploads.
   $ft["settings"]["CREATE"]            = TRUE; // Set to FALSE if you want to disable file/folder/url creation.
-  $ft["settings"]["FILEACTIONS"]       = TRUE; // Set to FALSE if you want to disable file actions (rename, move, delete, edit, duplicate).
+  $ft["settings"]["FILEACTIONS"]       = FALSE; // Set to FALSE if you want to disable file actions (rename, move, delete, edit, duplicate).
   $ft["settings"]["HIDEFILEPATHS"]     = FALSE; // Set to TRUE to pass downloads through File Thingie.
   $ft["settings"]["DELETEFOLDERS"]     = FALSE; // Set to TRUE to allow deletion of non-empty folders.
   $ft["settings"]["SHOWDATES"]         = FALSE; // Set to a date format to display last modified date (e.g. 'Y-m-d'). See http://dk2.php.net/manual/en/function.date.php
   $ft["settings"]["FILEBLACKLIST"]     = "ft2.php ft.css config.php index.php config.sample.php LICENSE README.markdown .DS_store .gitignore"; // Specific files that will not be shown.
   $ft["settings"]["FOLDERBLACKLIST"]   = "plugins js css locales data"; // Specifies folders that will not be shown. No starting or trailing slashes!
-  $ft["settings"]["FILETYPEBLACKLIST"] = "php phtml php3 php4 php5"; // File types that are not allowed for upload.
+  $ft["settings"]["FILETYPEBLACKLIST"] = "php phtml php3 php4 php5 sql"; // File types that are not allowed for upload.
   $ft["settings"]["FILETYPEWHITELIST"] = ""; // Add file types here to *only* allow those types to be uploaded.
   $ft["settings"]["ADVANCEDACTIONS"]   = FALSE; // Set to TRUE to enable advanced actions like chmod and symlinks.
   $ft["settings"]["LIMIT"]             = 0; // Restrict total dir file usage to this amount of bytes. Set to "0" for no limit.
